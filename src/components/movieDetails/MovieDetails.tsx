547aa9b84999ffe27a7d49dfa823e689e886c111
Juliasavransky <63636407+Juliasavransky@users.noreply.github.com>
2023-06-02 14:49:13 +0300

    selected Movie Details in a selected Movie Details slice

--- src/components/movieDetails/MovieDetails.tsx
+++ src/components/movieDetails/MovieDetails.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  asyncFetchSelectedMoviesFromApi,
+  getSelectedMovie,
+} from '../../features/movies/selectedSlice';
 
 function MovieDetails() {
+  const params = useParams();
+  const movieId = Number(params.id);
+
+  const dispatch = useDispatch();
+  const selectedMovieDetails = useSelector(getSelectedMovie);
+
+  const test = selectedMovieDetails.selectedMovie;
+  console.log(test);
+
+  useEffect(() => {
+    //@ts-ignore
+    dispatch(asyncFetchSelectedMoviesFromApi(movieId));
+  }, [dispatch, movieId]);
+
   return <div>MovieDetails</div>;
 }
 
